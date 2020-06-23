2a79ad500f9068c0950f0dff783be4ff3acc91ed
Ekaterina Kryakushina <ekaterina.kryakushina@yandex.ru>
2020-06-23 23:13:43 +0300

    SignUpCard, before deleting commented prev code

--- src/features/authorization/view/containers/SignUpCard/SignUpCard.tsx
+++ src/features/authorization/view/containers/SignUpCard/SignUpCard.tsx
@@ -5,15 +5,10 @@ import { Form, FormRenderProps } from 'react-final-form';
 import { autobind } from 'core-decorators';
 
 import { withTranslation, ITranslationProps, tKeys } from 'services/i18n';
-import { TextInputField, NumberInputField } from 'shared/view/form';
+import { TextInputField } from 'shared/view/form';
 import { Button } from 'shared/view/elements';
 import { IAppReduxState } from 'shared/types/app';
-import { IProfile } from 'shared/types/models';
-import { actionCreators as notificationActionCreators } from 'services/notification';
-// import {
-//   fieldNames, validateName, validateNickname, validateBio,
-// } from './constants';
-//import { ProfileAvatar } from '../../components';
+
 import { ISignUp, ISignUpPayload } from '../../../namespace';
 import { actionCreators, selectors } from './../../../redux';
 
@@ -43,31 +38,93 @@ function mapState(state: IAppReduxState): IStateProps {
 const b = block('sign-up-card');
 const { authorization: intl } = tKeys.features;
 
+const helper = (<div>Подсказка</div>);
+
 class SignUpCardComponent extends React.PureComponent<IProps> {
   public render() {
     const { isUserSigningUp, t } = this.props;
 
     return (
-      <form  onSubmit={this.handleSubmit}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <div className={b('button')}>
-          <Button variant="outlined" type="submit">{t(tKeys.features.authorization.buttonSignUp)}</Button>
-        </div>
-      </form>
+      <Form
+        onSubmit={this.handleFormSubmit}
+        render={this.renderForm}
+        subscription={{}}
+      />
+      // <form className={b()} onSubmit={this.handleSubmit}>
+      //   <div className={b('email')}>
+      //     <TextInputField
+      //       name='test'
+      //       required={true}
+      //       type='email'
+      //       label='Email'
+      //       //helperText={helper}
+      //       t={t}
+      //     />
+      //   </div>
+      //   <label>
+      //     Password
+      //     <input name="password" type="password" placeholder="Password" />
+      //   </label>
+      //   <div className={b('button')}>
+      //     <Button 
+      //       variant="outlined" 
+      //       type="submit">
+      //       {t(tKeys.features.authorization.buttonSignUp)}
+      //     </Button>
+      //   </div>
+      // </form>
     );
   }
 
+  // @autobind
+  // private handleSubmit(event: React.SyntheticEvent) {
+  //   //event.preventDefault();
+  //   this.props.signUp({email: 'test@test.ru', password: 'YTRYTbvnvgv3244!!'});
+  // }
+
   @autobind
-  private handleSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    this.props.signUp({email: 'test@test.ru', password: 'YTRYTbvnvgv3244!!'});
+  private handleFormSubmit(values: ISignUpPayload) {
+    console.log(values);
+    //event.preventDefault();
+    //this.props.signUp(values);
+  }
+
+  @autobind
+  private renderForm({ handleSubmit }: FormRenderProps) {
+    const { t } = this.props;
+    return (
+      <form className={b()} onSubmit={handleSubmit}>
+        <div className={b('text-input-field', 'email')}>
+          <TextInputField
+            name='email'
+            required={true}
+            type='email'
+            label='Email'
+            t={t}
+          />
+        </div>
+
+        <div className={b('text-input-field', 'email')}>
+          <TextInputField
+            name='password'
+            required={true}
+            type='password'
+            label='Password'
+            //helperText={helper}
+            t={t}
+          />
+        </div>
+
+        <div className={b('button')}>
+          <Button 
+            variant="outlined" 
+            type="submit"
+          >
+            {t(tKeys.features.authorization.buttonSignUp)}
+          </Button>
+        </div>
+      </form>
+    )
   }
 }
 
